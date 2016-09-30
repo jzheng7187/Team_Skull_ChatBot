e5f415a3659ba52010ffcbcc0846612c3e094bb4
Student 6 <Student 6@10.8.33.102>
2016-09-30 12:22:40 -0400

    added more class

---
+++ Team_Skull_ChatBot/src/groupFiles/JonathanPsychological.java
@@ -0,0 +1,22 @@
+package groupFiles;
+
+public class JonathanPsychological implements Topic {
+
+	private boolean inQuestionLoop;
+	private String qestionResponse;
+		
+	public void talk() {
+		
+	}
+
+	public boolean isTriggered(String userInput) {
+		if(JonathanMain.findKeyword(userInput, "depression" , 0) >= 0){
+			return true;
+		}
+		if(JonathanMain.findKeyword(userInput, "anxiety", 0) >= 0){
+			return true;
+		}
+		return false;
+	}
+
+}
