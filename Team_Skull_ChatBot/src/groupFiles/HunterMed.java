1cad8f291b6b2de231c9e65ea627d69b3de47f40
student 6 <student 6@10.8.33.157>
2016-09-30 12:13:36 -0400

    created class

---
+++ Team_Skull_ChatBot/src/groupFiles/HunterMed.java
@@ -0,0 +1,22 @@
+package groupFiles;
+
+public class HunterMed implements Topic {
+	
+	private boolean inMedLoop;
+	private String medResponse;
+	
+	public void talk(){
+		JonathanMain.print("What is your medical problem?");
+		inMedLoop = true;
+		while(inMedLoop){
+			medResponse = JonathanMain.getInput();
+		}
+	}
+	
+	public boolean isTriggered(String userInput){
+		if(JonathanMain.findKeyword(userInput, "medical", 0) >= 0){
+			return true;
+		}
+		return false;
+	}
+}
