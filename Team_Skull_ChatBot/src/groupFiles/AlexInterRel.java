fa3e6ccd675a1cb1dfcb4c563035b6d32cf4b8a6
Student 6 <Student 6@10.8.33.152>
2016-09-30 12:18:31 -0400

    LEl

---
+++ Team_Skull_ChatBot/src/groupFiles/AlexInterRel.java
@@ -0,0 +1,26 @@
+package groupFiles;
+
+
+
+public class AlexInterRel implements Topic {
+	private boolean inQuestionLoop ;
+	private String questionResponse;
+	private static String[] questions = {"What brings you here?", 
+			"How are you doing?"};
+
+public boolean isTriggered(String userInput) {
+	String[] triggers = {"","",""};
+	if(JonathanMain.findKeyword(userInput, "", 0) >=0 ){
+		return true;
+	}
+	if(JonathanMain.findKeyword(userInput, "", 0) >=0 ){
+		return true;
+	}
+	return false;
+}
+
+public void talk() {
+	
+}
+}
+
