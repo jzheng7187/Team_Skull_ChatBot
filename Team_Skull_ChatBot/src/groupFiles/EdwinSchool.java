1b5dd7fb544a8a5c2ed0ec9735905fb84f766d34
Edwin1 <Edwin1@Edwin-PC.home>
2016-10-04 09:51:44 -0400

    did some stuff

--- Team_Skull_ChatBot/src/groupFiles/EdwinSchool.java
+++ Team_Skull_ChatBot/src/groupFiles/EdwinSchool.java
@@ -1,14 +1,20 @@
 package groupFiles;
 
 public class EdwinSchool implements Topic {
-	
+	private boolean inSchoolLoop;
+	private String schoolResponse;
+	private String []answers = {"I understand that your problems are school related. But I first need some information "
+			+ "regarding your academics "+ JonathanMain.users +". What is your current GPA? "};
 	public void talk() {
+		inSchoolLoop = true;
+		while(inSchoolLoop){
 
-		
+			
+		}
 	}
 
 	public boolean isTriggered(String userInput) {
-		String []triggers = {"","","",""};
+		String []triggers = {"school","academic","college","major","GPA"};
 		for (int i = 0;i > triggers.length;i++ ){
 			if (JonathanMain.findKeyword(userInput,triggers[i],0) >= 0){
 				return true;
@@ -16,5 +22,11 @@ public class EdwinSchool implements Topic {
 		}
 		return false;
 	}
-
+	private boolean isaGPAResponse(String userInput){
+		double GPA = Double.parseDouble(userInput); 
+		if(GPA >= 5.0 || GPA<= 0.0){
+			return false;
+		}
+		return true;
+	}
 }
