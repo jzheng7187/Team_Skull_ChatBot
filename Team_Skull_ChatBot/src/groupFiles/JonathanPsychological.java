961d5eb37e557da762592996cb5125a2a87139c2
jzheng7187 <jzheng7187@bths.edu>
2016-10-04 15:06:08 -0400

    talk method

--- Team_Skull_ChatBot/src/groupFiles/JonathanPsychological.java
+++ Team_Skull_ChatBot/src/groupFiles/JonathanPsychological.java
@@ -7,7 +7,29 @@ public class JonathanPsychological implements Topic {
 	//food
 		
 	public void talk() {
-		
+		JonathanMain.print("What is your problem?");
+		inQuestionLoop = true;
+		while(inQuestionLoop){
+			questionResponse = JonathanMain.getInput();
+			int psychPsn = JonathanMain.findKeyword(questionResponse, "pyschological", 0);
+			if(psychPsn >= 0){
+				String psychProblem = questionResponse.substring(psychPsn + 14);
+				JonathanMain.print("I see that you have a " + psychProblem + "problem.")'
+				if(JonathanMain.findKeyword(questionResponse, "medical", 0)){
+					inQuestionLoop = false;
+					JonathanMain.HunterMed.talk();
+				}else if(JonathanMain.findKeyword(questionResponse, "inter-Relationship", 0){
+					 inQuestionLoop = false;
+					 JonathanMain.AlexInterRel.talk();
+				}else if(JonathanMain.findKeyword(questionResponse, "school", 0){
+					 inQuestionLoop = false;
+					 JonathanMain.EdwinSchool.talk();
+				else{
+					inQuestionLoop = false;
+					JonathanMain.talkForever();
+			else{
+				JonathanMain.print("I don't understand what your problem is");
+		}
 	}
 
 	public boolean isTriggered(String userInput) {
