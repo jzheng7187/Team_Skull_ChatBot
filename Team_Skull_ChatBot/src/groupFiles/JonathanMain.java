9b52bfbab7d1227a4654453190ddefd0e5a7f9b6
Student 6 <Student 6@10.8.33.102>
2016-10-05 12:28:03 -0400

    more stuff

--- Team_Skull_ChatBot/src/groupFiles/JonathanMain.java
+++ Team_Skull_ChatBot/src/groupFiles/JonathanMain.java
@@ -30,7 +30,7 @@ public class JonathanMain {
 	public static void talkForever(){
 		inLoop = true;
 		while(inLoop){
-			print("Greetings, " + users +".  What kind of problem do you  have? Is medical, psychological,school or inter-relationships?");
+			print("Greetings, " + users +".  What kind of problem do you  have? Is it medical, psychological,school or inter-relationships?");
 			response = getInput();
 			if(school.isTriggered(response)){
 				inLoop = false;
