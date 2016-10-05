9d3b4e8cfc8db232b24396189374df255cbf92af
Hunter <Hunter@Hunter-HP>
2016-10-05 00:29:05 -0400

    nothing

--- Team_Skull_ChatBot/src/groupFiles/HunterMed.java
+++ Team_Skull_ChatBot/src/groupFiles/HunterMed.java
@@ -4,18 +4,39 @@ public class HunterMed implements Topic {
 	
 	private boolean inMedLoop;
 	private String medResponse;
+	private int age;
+	private double height;
+	private double weight;
 	
 	public void talk(){
-		JonathanMain.print("What is your medical problem?");
 		inMedLoop = true;
 		while(inMedLoop){
 			JonathanMain.print("Greetings " +JonathanMain.users+", what is your age?");
+			medResponse = JonathanMain.getInput();
+			if(medResponse.indexOf("stop") >= 0){
+				inMedLoop = false;
+				JonathanMain.talkForever();
+			}
+			JonathanMain.print("What is your height?");
+			medResponse = JonathanMain.getInput();
+			if(medResponse.indexOf("stop") >= 0){
+				inMedLoop = false;
+				JonathanMain.talkForever();
+			}
+			JonathanMain.print("What is your weight?");
+			medResponse = JonathanMain.getInput();
+			if(medResponse.indexOf("stop") >= 0){
+				inMedLoop = false;
+				JonathanMain.talkForever();
+			}
 		}
 	}
 	
 	public boolean isTriggered(String userInput){
-		if(JonathanMain.findKeyword(userInput, "medical", 0) >= 0){
-			return true;
+		String []triggers = {"medical"};
+		for (int i = 0; i < triggers.length; i++)
+			if(JonathanMain.findKeyword(userInput, triggers[i], 0) >= 0){
+				return true;
 		}
 		return false;
 	}
