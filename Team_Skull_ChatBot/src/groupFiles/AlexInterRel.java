04a37a07cd33d7e8d63da8fccee14c0b6b396222
user <user@192.168.1.4>
2016-10-06 00:17:38 -0400

    did stuff

--- Team_Skull_ChatBot/src/groupFiles/AlexInterRel.java
+++ Team_Skull_ChatBot/src/groupFiles/AlexInterRel.java
@@ -2,21 +2,27 @@ package groupFiles;
 
 import java.util.Scanner;
 
+import chatbot.JonathanMain;
+import chatbot.Main;
+
 public class AlexInterRel implements Topic {
 	private boolean inQuestionLoop ;
 	static Scanner input;
 	static boolean whereLoop; 
 	static String response1;
+	static String response2;
 	private String problemResponse;
+	static boolean whoLoop;
+	static String who;
 	
 
 	private int problemCount;
 	
 	
 	private static String[] questions = {"What brings you here?", 
-			"What's the problem?"};
+			"What's the problem?","What's the matter?"};
 	private static String[] responses = {"Please tell me more.",
-			"I'm sorry to hear that.","Don't worry everything will be ok."};
+			"I'm sorry to hear that. ","Don't worry everything will be ok."};
 	public AlexInterRel(){
 		problemCount = 0;
 	}
@@ -25,20 +31,47 @@ public class AlexInterRel implements Topic {
 	{
 		inQuestionLoop = true;
 		while(inQuestionLoop){
-			problemCount++;
-			printResponse();
-			problemResponse = JonathanMain.getInput();
-			if(!isTriggered(problemResponse)){
-				inQuestionLoop = false;
-				
+			JonathanMain.print(JonathanMain.users+ "  "+"who do you not get along with?" );
+			response1 = JonathanMain.getInput();
+			if(response1.indexOf("stop")>=0){
+				inQuestionLoop =false;
+				JonathanMain.talkForever();
 			}
+			
+			talkProblem();
+			//if(printResponse.indexOf("stop")>=0){
+		//		inSchoolLoop =false;
+		//	problemResponse = JonathanMain.getInput();
+		//	if(!isTriggered(problemResponse)){
+			//	inQuestionLoop = false;
+				
+			
 		}
 	}
 	
-	public static void talkProblem(){
-   		
-			System.out.println("Who is causing this distress?");
-		   
+	        public static void talkProblem(){
+   			
+	        
+	        	System.out.println("Who is causing this distress?");
+	        	who = input.nextLine();
+	        	
+	        	whoLoop = true;
+	        	while(whoLoop){
+	        		System.out.println("Has "+who+" hurt you physically?" );
+	        		response2 = JonathanMain.getInput();
+	        		if(JonathanMain.findKeyword(response2, "yes", 0)>=0){
+	        			JonathanMain.print("Don't worry, I will call the police for you.");
+	        		}
+	        		else{
+	        			JonathanMain.print("What has your "+who+" said that hurt you?");
+	        		}
+	        			
+	        		
+	        	}
+	        	
+			
+			
+			
 			String who = input.nextLine();
 		    
 			System.out.println("Has "+who+" hurt you physically?" );
@@ -48,6 +81,7 @@ public class AlexInterRel implements Topic {
 			}
 			else{
 				System.out.println("What did "+who+" say that hurt you?");
+				
 			}
 			
 			
@@ -55,7 +89,7 @@ public class AlexInterRel implements Topic {
 	
 private void printResponse() {
 		int responseChoice = 0;
-		if(problemCount > 2 ){
+		if(problemCount > 3 ){
 			responseChoice = (int)(Math.random()*questions.length);
 			JonathanMain.print(questions[responseChoice]);
 		}
@@ -76,12 +110,12 @@ public boolean isTriggered(String userInput) {
 	if(JonathanMain.findKeyword(userInput, "relationship", 0) >=0 ){
 		return true;
 	}
+	if(JonathanMain.findKeyword(userInput, "inter-relationships", 0) >=0 ){
+		return true;
+	}
+	
 	
 	return false;
 }
 
 }
-
-
-
-
