29de6d6dfc0bcfe8b40c2d0373caecd8fe670d67
Edwin1 <Edwin1@Edwin-PC.home>
2016-10-05 21:51:05 -0400

    added lots of stuff

--- Team_Skull_ChatBot/src/groupFiles/EdwinSchool.java
+++ Team_Skull_ChatBot/src/groupFiles/EdwinSchool.java
@@ -20,84 +20,139 @@ public class EdwinSchool implements Topic {
 				schoolResponse = JonathanMain.getInput();
 				identifyProblem(schoolResponse);
 			}
-		if (chatCount > 1){
-			JonathanMain.print("Are there any other school related problems to be adressed " +name+"?");
-			String []negative = {"no","not","none","nothing","nah"};//negative responses
-			schoolResponse = JonathanMain.getInput();
+			else if (chatCount > 1){
+				JonathanMain.print("Are there any other school related problems to be adressed " +name+"?");
+				String []negative = {"no","not","none","nothing","nah"};//negative responses
+				schoolResponse = JonathanMain.getInput();
 
-			for (int i = 0;i < negative.length;i++ ){
-				if (JonathanMain.findKeyword(schoolResponse,negative[i],0) >= 0){
-					JonathanMain.print("Thanks for talking with the school branch! I will now kick you out of this"
-							+ "group chat and send you back to the main office.");
-					inSchoolLoop = false;
-					JonathanMain.talkForever();
+				for (int i = 0;i < negative.length;i++ ){
+					if (JonathanMain.findKeyword(schoolResponse,negative[i],0) >= 0){
+						JonathanMain.print("Thanks for talking with the school branch! I will now kick you out of this"
+								+ " group chat and send you back to the main office.");
+						inSchoolLoop = false;
+						JonathanMain.talkForever();
+					}
 				}
+				schoolResponse = JonathanMain.getInput();
+				identifyProblem(schoolResponse);	
 			}
-			schoolResponse = JonathanMain.getInput();
-			identifyProblem(schoolResponse);	
+
 		}
 	}
-}
 
-public boolean isTriggered(String userInput) {
-	String []triggers = {"school","academic","college","major","GPA"};
-	for (int i = 0;i < triggers.length;i++ ){
-		if (JonathanMain.findKeyword(userInput,triggers[i],0) >= 0){
-			return true;
+	public boolean isTriggered(String userInput) {
+		String []triggers = {"school","academic","GPA"};
+		for (int i = 0;i < triggers.length;i++ ){
+			if (JonathanMain.findKeyword(userInput,triggers[i],0) >= 0){
+				return true;
+			}
 		}
-	}
-	return false;
-}
-
-private void identifyProblem(String userInput) {
-	if (JonathanMain.findKeyword(userInput, "grades", 0) >= 0 || 
-			JonathanMain.findKeyword(userInput, "gpa", 0) >= 0 ){
-		gradesProblem();
-
-	}
-	else if (JonathanMain.findKeyword(userInput, "homework", 0) >= 0){
-		homeworkProblem();
-	}
-}
-
-//sdadsdsdsd
-private void homeworkProblem() {
-
-
-}
-
-private void gradesProblem() {
-	JonathanMain.print("What is your current GPA?");
-	schoolResponse = JonathanMain.getInput();
-	while(!isaGPAResponse(schoolResponse)){
-		JonathanMain.print("That is not a GPA response. Please type a proper GPA, using the CollegeBoard guidelines.");
-		schoolResponse = JonathanMain.getInput();
-	}
-	JonathanMain.print(GPA + " is a very high GPA!");
-
-
-}
-
-
-private boolean isaGPAResponse(String userInput){
-	if (isDouble(userInput)){
-		GPA = Double.parseDouble(userInput); 
-		if(GPA >= 5.0 || GPA<= 0.0){
-			return false;
-		}
-		return true;
-	}
-	return false;
-}
-//took this from stack overflow
-private boolean isDouble(String userInput) {
-	try{
-		Double.parseDouble(userInput);
-		return true;
-	}
-	catch(NumberFormatException e){
 		return false;
 	}
-}
+
+	private void identifyProblem(String userInput) {
+		boolean schoolProblem = false;
+		while(!schoolProblem){
+			if (JonathanMain.findKeyword(userInput, "grade", 0) >= 0 || 
+					JonathanMain.findKeyword(userInput, "gpa", 0) >= 0 ||
+					JonathanMain.findKeyword(userInput, "grades", 0) >= 0){
+				schoolProblem = true;
+				gradesProblem();
+			}
+			else if (JonathanMain.findKeyword(userInput, "college", 0) >= 0){
+				schoolProblem = true;
+				homeworkProblem();
+			}
+			else{
+				JonathanMain.print("That is not a school-related problem. Please tell me a school related problem.");
+				schoolResponse = JonathanMain.getInput();
+			}
+		}
+	}
+
+	private void homeworkProblem() {
+
+
+	}
+
+	private void gradesProblem() {
+		JonathanMain.print("What is your current GPA?");
+		schoolResponse = JonathanMain.getInput();
+		while(!isaGPAResponse(schoolResponse)){
+			JonathanMain.print("That is not a GPA response. Please type a proper GPA, using the CollegeBoard guidelines.");
+			schoolResponse = JonathanMain.getInput();
+		}
+		if (GPA >= 3.4){
+			JonathanMain.print("Your GPA is already quite high "+name+". However, you should strive for a higher GPA.");
+			//creates a amount of time for studying based on current GPA
+			int i = (int)Math.floor(GPA);
+			i = (int)(100*(1-(GPA - i)));
+			JonathanMain.print("I reccomend you study for at least "+i+" more minutes a day to ensure you understand whats happending in class."); 	
+		}
+		else{
+			findAPForHelp();
+		}
+	}
+
+
+	private void findAPForHelp() {
+		String []courses = {"algebra","geometry","trigonometry","calculus","apliterature","americanliterature","englishliterature",
+				"worldliterature","biology","chemistry","physics","worldhistory","us history","europeanhistory","economics"};
+		//using the array of courses, code attempts to provide an AP for help
+		JonathanMain.print("Hmm. This looks like a serious problem. Which specific class do you need help with?");
+		schoolResponse = JonathanMain.getInput();
+		for (int i = 0; i < courses.length;i++ ){
+			if ((JonathanMain.findKeyword(schoolResponse, courses[i], 0)>= 0)){
+				if(i < 4){
+					JonathanMain.print("If you are struggling with an Math related course, you can find the Math AP Mr.Smith"
+							+ " in room 6W14.");
+					return;
+				}
+				else if (i < 7){
+					JonathanMain.print("If you are struggling with an English related course, you can find the English AP Ms.Bonilla"
+							+ " in room 5W23.");
+					return; 
+				}
+				else if (i == 8 ){
+					JonathanMain.print("If you are struggling with a Biology course, you can always the Biology AP Ms.Lovelett"
+							+ " using the phone number (718)804-6400.");
+					return;
+				}
+				else if (i < 11){
+					JonathanMain.print("If you are struggling with a Physical Science course, you can contact the Physical Science AP Mr.Evangelist"
+							+ " using the phone number (718)804-6561.");
+					return;
+				}
+				else if (i < courses.length){
+					JonathanMain.print("If you are struggling with a Social Studies course, you can contact the Social Studies AP Mr.Newman"
+							+ "at DNewman@schools.nyc.gov");
+					return;
+				}
+			}
+		}
+		JonathanMain.print("I am sorry, but it appears this course doesn't exist in my catalog. "
+				+ "But whenever you're struggling, you can always go to the NHS tutoring sessions.");
+	}
+	//is the response a valid GPA
+	private boolean isaGPAResponse(String userInput){
+		if (isDouble(userInput)){
+			GPA = Double.parseDouble(userInput); 
+			if(GPA >= 4.0 || GPA<= 0.0){
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+	//took this from stack overflow
+	private boolean isDouble(String userInput) {
+		try{
+			Double.parseDouble(userInput);
+			return true;
+		}
+		catch(NumberFormatException e){
+			return false;
+		}
+	}
 
 }
