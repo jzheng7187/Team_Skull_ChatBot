087001ab2ad43a9c48519074efd8daa431db32d4
Hunter <Hunter@Hunter-HP>
2016-10-07 01:25:05 -0400

    finished i think

--- Team_Skull_ChatBot/src/groupFiles/HunterMed.java
+++ Team_Skull_ChatBot/src/groupFiles/HunterMed.java
@@ -7,12 +7,9 @@ public class HunterMed implements Topic {
 	private boolean heightLoop;
 	private boolean weightLoop;
 	private String medResponse;
-	private int age;
-	private double height;
-	private double weight;
-	private static double amount;
-	private static String medicine;
-	private int count;
+	private static int age;
+	private static double height;
+	private static double weight;
 
 	public void talk(){
 		inMedLoop = true;
@@ -23,63 +20,113 @@ public class HunterMed implements Topic {
 			JonathanMain.print("Greetings " +JonathanMain.users+", what is your medical problem?");
 			medResponse = JonathanMain.getInput();
 			int medPsn = JonathanMain.findKeyword(medResponse, "problem", 0);
-			if(JonathanMain.findKeyword(medResponse, "stop", 0) >= 0){
-				inMedLoop = false;
-				JonathanMain.talkForever();
-			}
-			if(medPsn >= 0){
-				String medicalProblem = medResponse.substring(medPsn+11);
-				JonathanMain.print("I will help you with your "+medicalProblem);
-			}
-			else{
-				JonathanMain.print("I will help you with your "+medResponse);
-			}
-			while(ageLoop){
-				JonathanMain.print("What is your age in years?");
-				medResponse = JonathanMain.getInput();
-				if(JonathanMain.findKeyword(medResponse, "stop", 0) >= 0){
+			String [] stop = {"stop","no","not","never","nobody","none","nothing","nowhere"};
+			for (int i = 0; i < stop.length; i++)
+				if(JonathanMain.findKeyword(medResponse,stop[i], 0) >= 0){
 					inMedLoop = false;
 					JonathanMain.talkForever();
 				}
-				count = 0;
-				String[] numbers = {"0","1","2","3","4","5","6","7","8","9"};
-				for (int i = 0; i < medResponse.length(); i++)
-					for(int n = 0; n < 10; n++)
-						if(JonathanMain.findKeyword(medResponse, numbers[n], 0) >= 0){
-							ageLoop = false;
-							count++;
-						}
-				if(ageLoop == false){
-					age = Integer.parseInt(medResponse.substring(0,count));
-				}
-				if(ageLoop == true){
-					JonathanMain.print("I don't understand you.");
-				}
+			if(medPsn >= 0){
+				String medicalProblem = medResponse.substring(medPsn+11);
+				JonathanMain.print("I will help you with your "+medicalProblem);
+				askAge();
 			}
-			JonathanMain.print("What is your height in meters?");
-			medResponse = JonathanMain.getInput();
-			if(medResponse.indexOf("stop") >= 0){
-				inMedLoop = false;
-				JonathanMain.talkForever();
+			else{
+				JonathanMain.print("I don't understand you");
 			}
-			height = Double.parseDouble(medResponse);
-			JonathanMain.print("What is your weight in kilograms?");
-			medResponse = JonathanMain.getInput();
-			if(medResponse.indexOf("stop") >= 0){
-				inMedLoop = false;
-				JonathanMain.talkForever();
-			}
-			weight = Double.parseDouble(medResponse);
-			JonathanMain.print(JonathanMain.users+", I think you need "+amount+"mg of "+medicine+" for your problem.");
 		}
 	}
 
-	private static double getAmount(){
-		return amount;
+	private void askAge() {
+		while(ageLoop){
+			JonathanMain.print("What is your age in years?");
+			medResponse = JonathanMain.getInput();
+			if(JonathanMain.findKeyword(medResponse, "stop", 0) >= 0){
+				ageLoop = false;
+				JonathanMain.talkForever();
+			}
+			int agePsn = JonathanMain.findKeyword(medResponse, "am", 0);
+			int yearsPsn = JonathanMain.findKeyword(medResponse, "years", 0);
+			if(agePsn >= 0 && yearsPsn >=0){
+				age = Integer.parseInt(medResponse.substring(agePsn+3,yearsPsn-1));
+				JonathanMain.print(ageComment());
+				askHeight();
+			}
+			else JonathanMain.print("I don't understand you.");
+		}
 	}
 
-	private static String getMedicine(){
-		return medicine;
+	private void askHeight() {
+		while(heightLoop){
+			JonathanMain.print("What is your height in meters?");
+			medResponse = JonathanMain.getInput();
+			if(JonathanMain.findKeyword(medResponse, "stop", 0) >= 0){
+				heightLoop = false;
+				JonathanMain.talkForever();
+			}
+			int heightPsn = JonathanMain.findKeyword(medResponse, "am", 0);
+			int metersPsn = JonathanMain.findKeyword(medResponse, "meters", 0);
+			if(heightPsn >= 0 && metersPsn >=0){
+				height = Double.parseDouble(medResponse.substring(heightPsn+3,metersPsn-1));
+				JonathanMain.print(heightComment());
+				askWeight();
+			}
+			else JonathanMain.print("I don't understand you.");
+		}
+	}
+
+	private void askWeight() {
+		while(weightLoop){
+			JonathanMain.print("What is your weight in kilograms?");
+			medResponse = JonathanMain.getInput();
+			if(JonathanMain.findKeyword(medResponse, "stop", 0) >= 0){
+				weightLoop = false;
+				JonathanMain.talkForever();
+			}
+			int weighPsn = JonathanMain.findKeyword(medResponse, "weigh", 0);
+			int kilosPsn = JonathanMain.findKeyword(medResponse, "kilograms", 0);
+			if(weighPsn >= 0 && kilosPsn >=0){
+				weight = Double.parseDouble(medResponse.substring(weighPsn+6,kilosPsn-1));
+				JonathanMain.print(weightComment());
+				advice();
+			}
+			else JonathanMain.print("I don't understand you.");
+		}
+	}
+
+	private String ageComment(){
+		if (age < 30){
+			return ("Wow, you are young.");
+		}
+		else return ("Wow, you are old.");
+	}
+
+	private String heightComment() {
+		if (height < 1.8){
+			return ("Wow, you are short.");
+		}
+		else return ("Wow, you are tall.");
+	}
+
+	private String weightComment() {
+		if (weight < 64){
+			return ("Wow, you are light.");
+		}
+		else return ("Wow, you are heavy.");
+	}
+
+	private void advice() {
+		JonathanMain.print(JonathanMain.users+", I think you need "+getAmount()+"mg of "+getMedicine()+" for your problem.");
+		JonathanMain.talkForever();
+	}
+
+	private String getMedicine() {
+		String [] medicine = {"fat","carbohydrate","protien","cholesterol","sodium"};
+		return medicine[(int)(Math.random() * medicine.length)];
+	}
+
+	private static double getAmount(){
+		return (age+(height*2)+(weight/2));
 	}
 
 	public boolean isTriggered(String userInput){
